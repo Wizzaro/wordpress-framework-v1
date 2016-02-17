6a593e5d35fba24389c81004983802091e1e7c9f
Przemek <przemyslaw.dziadek@gmail.com>
2016-02-17 16:24:18 +0100

    add DIRECTORY_SEPARATOR for theme bootstrap

--- Bootstrap/AbstractThemeBootstrap.php
+++ Bootstrap/AbstractThemeBootstrap.php
@@ -7,7 +7,7 @@ abstract class AbstractThemeBootstrap extends AbstractBootstrap {
     
     public function get_dir_patch() {
         if ( ! isset( $this->_config['path']['dir'] ) ) {
-            $this->_config['path']['dir'] = get_template_directory();
+            $this->_config['path']['dir'] = get_template_directory() . DIRECTORY_SEPARATOR;
         }
         
         return $this->_config['path']['dir'];
@@ -15,7 +15,7 @@ abstract class AbstractThemeBootstrap extends AbstractBootstrap {
     
     public function get_dir_url() {
         if ( ! isset( $this->_config['path']['url'] ) ) {
-            $this->_config['path']['url'] = get_template_directory_uri();
+            $this->_config['path']['url'] = get_template_directory_uri() . DIRECTORY_SEPARATOR;
         }
         
         return $this->_config['path']['url'];
