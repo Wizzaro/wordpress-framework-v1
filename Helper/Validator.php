84e9d516cd33be3dc159bab8c749b2cffa9ed71c
Przemek <przemyslaw.dziadek@gmail.com>
2016-02-21 16:33:54 +0100

    no static validator function

--- Helper/Validator.php
+++ Helper/Validator.php
@@ -5,7 +5,7 @@ use Wizzaro\WPFramework\v1\AbstractSingleton;
 
 class Validator extends AbstractSingleton {
     
-    public static function validate_min_max_int( $int, $min = null, $max = null, $default = false ) {
+    public function validate_min_max_int( $int, $min = null, $max = null, $default = false ) {
         if( is_numeric( $int ) ) {
             if( ( ! is_numeric( $min ) || ( $int >= $min ) ) && ( ! is_numeric( $max ) || ( $int <= $max ) ) ) {
                 return $int;
